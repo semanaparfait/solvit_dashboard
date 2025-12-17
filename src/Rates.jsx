986d8b31b67d9afae4f0bfddaef0e288686eff93
semanaparfait <p.semana@alustudent.com>
2025-12-17 13:11:51 +0200

    editing rates

--- src/Rates.jsx
+++ src/Rates.jsx
@@ -1,33 +1,43 @@
-import {Users,ShoppingCart,BarChart,Package} from 'lucide-react'
-import React from 'react'
+import { Users,  Box, TriangleAlert,CircleCheckBig } from 'lucide-react';
+import React from 'react';
 
 function Rates() {
-    const rates = [
-        { nber: 110, text: "total Users", icon: Users},
-        { nber: 100, text: "total products",  icon: ShoppingCart},
-        { nber: 10, text: "Assigned Products", icon: Package},
-        { nber: 90, text: "Unassigned Products", icon: BarChart},
-,
-    ];
+  const rates = [
+    { nber: 110, text: "Total Users", icon: Users },
+    { nber: 100, text: "Total Products", icon: Box },
+    { nber: 10, text: "Assigned Products", icon: CircleCheckBig },
+    { nber: 90, text: "Unassigned Products", icon: TriangleAlert },
+  ];
+
   return (
-        <div className="flex flex-wrap items-center justify-evenly gap-4 overflow-x-auto md:overflow-visible">
-        {rates.map((rate, index) => (
-            <div
+    <div className="flex flex-wrap items-center justify-evenly gap-4 overflow-x-auto md:overflow-visible">
+      {rates.map((rate, index) => {
+        const IconComponent = rate.icon;
+
+
+        let bgColor = " backdrop-blur-md"; 
+        if (IconComponent === Users) bgColor = "text-blue-500 bg-blue-500/20 backdrop-blur-md";
+        if (IconComponent === Box) bgColor = "text-blue-500 bg-blue-500/20 backdrop-blur-md";
+        if (IconComponent === CircleCheckBig) bgColor = "text-green-500 bg-green-500/20 backdrop-blur-md";
+        if (IconComponent === TriangleAlert) bgColor = "text-yellow-500 bg-yellow-500/20 backdrop-blur-md";
+
+        return (
+          <div
             key={index}
             className="min-w-[220px] p-4 shadow-md rounded-md flex items-center gap-4"
-            >
-            <div className="bg-primaryColor-500 p-2 rounded-md">
-                <rate.icon color="white" size={24} />
+          >
+            <div className={`p-3 rounded-md flex items-center justify-center ${bgColor}`}>
+              <IconComponent  size={28} />
             </div>
             <div>
-                <h2 className="font-bold text-[18px]">{rate.nber}</h2>
-                <p className="text-sm">{rate.text}</p>
+              <h2 className="font-bold text-[18px]">{rate.nber}</h2>
+              <p className="text-sm">{rate.text}</p>
             </div>
-            </div>
-        ))}
-        </div>
-
-  )
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default Rates
+export default Rates;
